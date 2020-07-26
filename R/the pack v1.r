58e3b1f3145b5fb590bb16cf2a5e7d2c84537a0c
pit1516 <40230116+pit1516@users.noreply.github.com>
2020-07-26 15:31:45 +0200

    now has a line for the mean (which needs double-checking!)

--- R/the pack v1.r
+++ R/the pack v1.r
@@ -146,19 +146,27 @@ datex.helper <- function(dat,id,dv,uv)
   mutate(point=ifelse(outlier==T,as.numeric(NA),!!sym(dv))) # outlier points
 }
 
-datex.ridge <- function(dat,dv,uv,alpha_density=0.5,alpha_hist=0.2,scale=0.9,bins=10,facet,hist=FALSE,draw_baseline=T,col_density="black",col_hist="black",level,rug=FALSE)
+datex.ridge <- function(dat,dv,uv,alpha_density=0.5,alpha_hist=0.2,scale=0.9,bins=10,facet,hist=FALSE,draw_baseline=T,col_density="black",col_hist="black",level,rug=FALSE,quantiles=FALSE)
 {
   if (!missing(level)) {dat <- dat[dat[,uv]==level,]}
   # !!! point_color is questionable... hard to see for some colors, maybe just leave that black?
   p <- dat %>% ggplot(aes(x=!!sym(dv),y=!!sym(uv),fill=!!sym(uv),point_color=!!sym(uv)))
   if (hist==TRUE) {p <- p + geom_density_ridges(color=col_hist,draw_baseline=draw_baseline,stat="binline",rel_min_height=0.0,scale=scale,alpha=alpha_hist,bins=bins)}
-  # ??? there HAS to be a better way, right ???
-  if (rug==FALSE) {p <- p + geom_density_ridges(color=col_density,rel_min_height=0.0,scale=scale,jittered_points=F,alpha=alpha_density)}
-  if (rug==TRUE) {p <- p + geom_density_ridges(color=col_density,rel_min_height=0.0,scale=scale,alpha=alpha_density,
-        jittered_points = TRUE,
-        point_shape = "|", point_size = 3, size = 0.25,
-        position = position_points_jitter(height = 0)
-  )}
+  p <- p + geom_density_ridges(
+    color=col_density,
+    rel_min_height=0.0,
+    scale=scale,
+    alpha=alpha_density,
+    jittered_points = rug,
+    point_shape = "|", 
+    point_size = 3, 
+    size = 0.25,
+    position = position_points_jitter(height = 0),
+    quantile_lines = quantiles, 
+    quantile_fun = mean,
+    vline_size=1,
+    aes(vline_color=!!sym(uv))
+  )
   if (!missing(facet)) {p <- p + facet_grid(cols=vars(!!sym(facet)),scales="free")}
   p <- p + theme(legend.position="none")
   p
